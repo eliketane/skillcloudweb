00f9f6d3ec1c08c4caee684616e264f633f0ff17
Jon Provan <jprovan@skillstorm.com>
2023-12-29 13:05:12 -0500

    Backend & Database Creation

---
+++ backend/sc-back/src/main/java/com/skillcloud/ScBackApplication.java
@@ -0,0 +1,13 @@
+package com.skillcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScBackApplication.class, args);
+	}
+
+}
---
+++ backend/sc-back/src/test/java/com/skillcloud/ScBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
